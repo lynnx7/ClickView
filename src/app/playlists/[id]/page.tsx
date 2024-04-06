3ee66c5ced819dd91109bf204652cfda8bcd266f
Jay <109907443+lynnx7@users.noreply.github.com>
2024-04-06 22:12:31 +1100

    create playlist/id page

---
+++ src/app/playlists/[id]/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useEffect, useState } from 'react';
+import VideoItem from '@/components/video-item';
+
+export default function PlaylistPage({ params }: { params: { id: string } }) {
+  const [videolist, setVideolist] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchPlaylist() {
+      try {
+
+        const response = await fetch('/api/playlists/'+params.id);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setVideolist(data);
+        console.log("refreshing")
+        
+      } catch (error) {
+        console.error('Error fetching playlist:', error);
+      }
+    }
+
+    if (params.id) {
+      fetchPlaylist();
+    }
+  }, [params.id]);
+
+  if (!videolist) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>This Playlist {params.id}</h1>
+
+      {videolist ? (
+        <div>
+        {videolist.map((video, index) => (
+          <VideoItem key={index} video={video}
+          />
+          
+        ))}
+        </div>
+      ) 
+       : (
+        'Loading...'
+      )}
+    </div>
+  );
+}
