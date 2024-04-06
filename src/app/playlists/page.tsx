c07540e675c3a400e98e07bc377708cd5381d9ad
Jay <109907443+lynnx7@users.noreply.github.com>
2024-04-06 21:58:34 +1100

    show pre-made route

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,9 +1,45 @@
+"use client";
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { PlaylistItem } from '@/components/playlist-item';
 
 export default function PlaylistsPage() {
+  const [playlist, setPlaylist] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/playlists');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        console.log("refresh",data)
+        
+        setPlaylist(data);
+      } catch (error) {
+        console.error('Error fetching data:');
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>Playlists route</h1>
+      
+      {playlist ? (
+        <div>
+        {playlist.map((list, index) => (
+          <PlaylistItem key={list.id} playlist={list} />
+          
+        ))}
+        </div>
+      ) 
+       : (
+        'Loading...'
+      )}
     </>
   );
 }
--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import VideoItem from '@/components/video-item';
+
 export default function VideosPage() {
+  const [videolist, setVideolist] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/videos');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        console.log(data)
+        setVideolist(data);
+      } catch (error) {
+        console.error('Error fetching data:');
+      }
+    }
+
+    fetchData();
+  }, []);
+
+
+
   return (
     <>
       <h1>Videos route</h1>
+      {videolist ? (
+        <div>
+
+        {videolist.map((video, index) => (
+          <VideoItem key={index} video={video} 
+          />
+        ))}
+        </div>
+      ) 
+       : (
+        'Loading...'
+      )}
     </>
   );
 }
